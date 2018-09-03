a38a922e25d4c974b4792491e2e2b526c809f0ae
Javier Romero <javier@cs.uni-potsdam.de>
2018-09-03 12:27:29 +0200

    first commit

---
+++ multiclingo.py
@@ -0,0 +1,125 @@
+#script (python)
+
+
+# imports
+import clingo
+
+
+# defines
+UNKNOWN = 0
+UNSATISFIABLE = 1
+SATISFIABLE = 2
+HOLDS = "holds"
+PREV = "prev"
+FIRST = "first"
+
+
+class ProgramGroundObserver:
+
+    def __init__(self, control, projection):
+        control.register_observer(self)
+        self.projection = True if projection==1 else False
+        if self.projection:
+            self.atoms = []
+        else:
+            self.atoms = set()
+
+    def project(self, atom):
+        if self.projection:
+            self.atoms.append(atom[0])
+
+    def rule(self, choice, head, body):
+        if not self.projection:
+            self.atoms.update(head)
+
+    def weight_rule(self, choice, head, lower_bound, body):
+        if not self.projection:
+            self.atoms.update(head)
+
+    def get_atoms(self):
+        if self.projection:
+            return self.atoms
+        else:
+            return sorted(list(self.atoms))
+
+
+class Controller:
+
+    def __init__(self, control, projection):
+        self.control = control
+        self.calls = 0
+        self.result = UNKNOWN
+        self.prev = []
+        self.last_prev = []
+        self.atoms = []
+        self.model = []
+        # observer and backend
+        self.observer = ProgramGroundObserver(control, projection)
+        self.backend = self.control.backend()
+
+    def ground(self, *args, **kwargs):
+        self.control.ground(*args, **kwargs)
+        self.atoms = self.observer.get_atoms()
+        del self.observer
+
+    def on_model(self, model):
+        # set result and print call number
+        self.result = SATISFIABLE
+        print("Call: {}".format(self.calls))
+        # set last_prev, prev and model
+        self.last_prev = self.prev
+        self.prev = [
+            atom.symbol.arguments
+            for atom in self.control.symbolic_atoms.by_signature(HOLDS, 1)
+            if atom.symbol in model.symbols(atoms=True)
+        ]
+        self.model = [i for i in self.atoms if model.is_true(i)]
+
+    def solve(self, *args, **kwargs):
+        # set external prev/1
+        for atom in self.last_prev:
+            self.control.assign_external(clingo.Function(PREV, atom), False)
+        for atom in self.prev:
+            self.control.assign_external(clingo.Function(PREV, atom),  True)
+        # set external first/0
+        if self.calls == 0:
+            self.control.assign_external(clingo.Function(FIRST, []),  True)
+        elif self.calls == 1:
+            self.control.release_external(clingo.Function(FIRST, []))
+        # add constraint for last model
+        if self.calls == 0:
+            self.backend.__enter__()
+        if self.calls > 0:
+            body = self.model + [-a for a in self.atoms if a not in self.model]
+            self.backend.add_rule([], body, False)
+        # solve and return result
+        self.result = UNSATISFIABLE
+        self.calls += 1
+        self.control.solve(on_model = self.on_model, *args, **kwargs)
+        return self.result
+
+
+#
+# main
+#
+
+def get(val, default):
+    return val if val != None else default
+
+def main(control):
+
+    # options
+    max_models = get(control.get_const("models"),  clingo.Number(1)).number
+    projection = get(control.get_const("project"), clingo.Number(0)).number
+
+    # loop
+    controller = Controller(control, projection)
+    controller.ground([("base", [])])
+    models = 0
+    while models < max_models or max_models == 0:
+        result = controller.solve()
+        if result == SATISFIABLE:
+            models += 1
+        else:
+            break
+#end.

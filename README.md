a79da206fb6192f0d1dc11e32de212af106ea917
Javier Romero <javier@cs.uni-potsdam.de>
2018-09-03 14:56:06 +0200

    working for optimization

--- README.md
+++ README.md
@@ -29,49 +29,80 @@ The program must define some externals:
 % base part
 %
 
-dom(1..5).
-{ a(X) : dom(X) }.
+dom(1..4).
+2 { a(X) : dom(X) }.
 #show a/1.
 
 { b(1) }.
 #show b/1.
 #project b/1.
 
+#minimize{
+  1@1,X: a(X), minimize=1;
+  1@2,X: b(X), minimize=1
+}.
 
 %
 % multi part
 %
 
 % heuristics using prev
-#heuristic a(X) :     prev(a(X)),         not first. [-1,sign]
-#heuristic a(X) : not prev(a(X)), dom(X), not first. [ 1,sign]
+#heuristic a(X) :     prev(a(X)),         not first. [1,false]
+#heuristic a(X) : not prev(a(X)), dom(X), not first. [1, true]
 
 % holds/1 definition
 holds(a(X)) :- a(X).
 
 % externals
-#external prev(a(X)) : dom(X).
 #external first.
+#external prev(a(X)) : dom(X).
 ```
 
-* Execution:
+* Execution (all models projecting on b/1):
 ```bash
-$ clingo multiclingo.py diverse.lp -c models=2 -c project=1 --heuristic=Domain
+$ clingo multiclingo.py diverse.lp -c models=0 -c project=1 --heuristic=Domain
 clingo version 5.3.0
 Reading from multiclingo.py ...
 Solving...
 Call: 1
 Answer: 1
-
+a(1) a(2)
 Solving...
 Call: 2
 Answer: 1
-a(1) a(2) a(3) a(4) a(5) b(1)
-SATISFIABLE
+a(3) a(4) b(1)
+Solving...
+UNSATISFIABLE
 
-Models       : 2+
+Models       : 2
+Calls        : 3
+Time         : 0.015s (Solving: 0.00s 1st Model: 0.00s Unsat: 0.00s)
+CPU Time     : 0.016s
+
+```
+
+* Execution (2 optimal models):
+```bash
+$ clingo multiclingo.py diverse.lp -c models=2 -c minimize=1 --heuristic=Domain
+clingo version 5.3.0
+Reading from multiclingo.py ...
+Solving...
+Call: 1
+Answer: 1
+a(1) a(2)
+Optimization: 0 2
+Solving...
+Call: 2
+Answer: 1
+a(3) a(4)
+Optimization: 0 2
+OPTIMUM FOUND
+
+Models       : 2
+  Optimum    : yes
+Optimization : 0 2
 Calls        : 2
-Time         : 0.020s (Solving: 0.00s 1st Model: 0.00s Unsat: 0.00s)
-CPU Time     : 0.020s
+Time         : 0.016s (Solving: 0.00s 1st Model: 0.00s Unsat: 0.00s)
+CPU Time     : 0.016s
 
 ```
--- multiclingo.py
+++ multiclingo.py
@@ -23,6 +23,21 @@ class ProgramGroundObserver:
             self.atoms = []
         else:
             self.atoms = set()
+        self.minimization = {}
+
+    def get_atoms(self):
+        if self.projection:
+            return self.atoms
+        else:
+            return sorted(list(self.atoms))
+
+    def get_minimization(self):
+        keys = sorted(self.minimization.keys(), reverse=True)
+        return [sorted(list(self.minimization[key])) for key in keys]
+
+    #
+    # observe
+    #
 
     def project(self, atom):
         if self.projection:
@@ -36,11 +51,12 @@ class ProgramGroundObserver:
         if not self.projection:
             self.atoms.update(head)
 
-    def get_atoms(self):
-        if self.projection:
-            return self.atoms
-        else:
-            return sorted(list(self.atoms))
+    def minimize(self, priority, literals):
+        level = self.minimization.get(priority)
+        if level is None:
+            level = set()
+            self.minimization[priority] = level
+        level.update(literals)
 
 
 class Controller:
@@ -53,6 +69,8 @@ class Controller:
         self.last_prev = []
         self.atoms = []
         self.model = []
+        self.cost = []
+        self.minimization = []
         # observer and backend
         self.observer = ProgramGroundObserver(control, projection)
         self.backend = self.control.backend()
@@ -60,13 +78,15 @@ class Controller:
     def ground(self, *args, **kwargs):
         self.control.ground(*args, **kwargs)
         self.atoms = self.observer.get_atoms()
+        self.minimization = self.observer.get_minimization()
         del self.observer
 
     def on_model(self, model):
         # set result and print call number
         self.result = SATISFIABLE
         print("Call: {}".format(self.calls))
-        # set last_prev, prev and model
+        # if optimization:
+        # set last_prev, prev, model and cost
         self.last_prev = self.prev
         self.prev = [
             atom.symbol.arguments
@@ -74,6 +94,7 @@ class Controller:
             if atom.symbol in model.symbols(atoms=True)
         ]
         self.model = [i for i in self.atoms if model.is_true(i)]
+        self.cost = model.cost
 
     def solve(self, *args, **kwargs):
         # set external prev/1
@@ -92,6 +113,18 @@ class Controller:
         if self.calls > 0:
             body = self.model + [-a for a in self.atoms if a not in self.model]
             self.backend.add_rule([], body, False)
+        # if calls == 1 and optimization: add constraint
+        if self.calls == 1 and self.cost != []:
+            # just in case:
+            if len(self.cost) != len(self.minimization):
+                raise Exception("ERROR (multiclingo): implementation error")
+            # iterate over levels
+            for idx, cost in enumerate(self.cost):
+                level = self.minimization[idx]
+                self.backend.add_weight_rule([], cost+1, level, False)
+                total = sum([abs(w) for _, w in level])
+                body = [(-l,w) for l, w in level]
+                self.backend.add_weight_rule([], total-cost+1, body, False)
         # solve and return result
         self.result = UNSATISFIABLE
         self.calls += 1
